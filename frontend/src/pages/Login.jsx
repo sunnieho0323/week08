4aee0e822382c96d4fc2074b8722b3913baf56c8
sunnieho0323 <s224863464@deakin.edu.au>
2026-09-14 14:56:18 +1000

    Enable continuous deployment and update frontend heading

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -93,7 +93,7 @@ const Login = () => {
             variant="h4"
             gutterBottom
           >
-            KoalaTech University
+            KoalaTech University - Week 09 Deployment Task
           </Typography>
 
           <Typography
